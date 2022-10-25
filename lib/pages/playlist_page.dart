25c9d8e8bc24dbf9d7f01ffd4f77ea9e908711a7
roaa94 <roaa94khaddam@gmail.com>
2022-10-26 00:51:21 +0300

    chore: work on playlist page

--- lib/pages/playlist_page.dart
+++ lib/pages/playlist_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playlist_animation/utils/hero_animation_manager.dart';
 import 'package:flutter_playlist_animation/widgets/image_wrapper.dart';
+import 'package:flutter_playlist_animation/widgets/song_action_buttons.dart';
 
 class PlaylistPage extends StatefulWidget {
   const PlaylistPage({
@@ -15,11 +16,21 @@ class PlaylistPage extends StatefulWidget {
 }
 
 class _PlaylistPageState extends State<PlaylistPage> {
-  late final Animation<double> offsetAnimation;
+  late final Animation<double> appBarOffsetAnimation;
+  late final Animation<Offset> contentOffsetAnimation;
 
   @override
   void initState() {
-    offsetAnimation = Tween(begin: -100.0, end: 0.0).animate(
+    appBarOffsetAnimation = Tween(begin: -100.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: widget.routeAnimation,
+        curve: const Interval(0.5, 1, curve: Curves.easeOut),
+      ),
+    );
+    contentOffsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: const Offset(0, 0),
+    ).animate(
       CurvedAnimation(
         parent: widget.routeAnimation,
         curve: const Interval(0.5, 1, curve: Curves.easeOut),
@@ -34,33 +45,69 @@ class _PlaylistPageState extends State<PlaylistPage> {
       body: Column(
         children: [
           AnimatedBuilder(
-            animation: offsetAnimation,
+            animation: appBarOffsetAnimation,
             builder: (context, child) {
               return Transform.translate(
-                offset: Offset(0, offsetAnimation.value),
+                offset: Offset(0, appBarOffsetAnimation.value),
                 child: AppBar(),
               );
             },
           ),
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(height: 50),
-                  Center(
-                    child: Hero(
-                      tag: 'image-hero',
-                      child: Transform(
-                        transform: HeroAnimationManager.endTransformMatrix,
-                        alignment: Alignment.center,
-                        child: const ImageWrapper(
-                          image: 'assets/images/image-4.jpeg',
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 240,
+                  automaticallyImplyLeading: false,
+                  floating: true,
+                  flexibleSpace: Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Center(
+                      child: Hero(
+                        tag: 'image-hero',
+                        child: Transform(
+                          transform: HeroAnimationManager.endTransformMatrix,
+                          alignment: Alignment.center,
+                          child: const ImageWrapper(
+                            image: 'assets/images/image-4.jpeg',
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate([
+                      SlideTransition(
+                        position: contentOffsetAnimation,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Center(
+                              child: Text(
+                                'Acoustic',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            SongActionButtons(),
+                            SizedBox(height: 20),
+                            Text(
+                              'Upcoming songs',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]),
+                  ),
+                ),
+              ],
             ),
           )
         ],
--- lib/widgets/image_wrapper.dart
+++ lib/widgets/image_wrapper.dart
@@ -4,17 +4,19 @@ class ImageWrapper extends StatelessWidget {
   const ImageWrapper({
     super.key,
     required this.image,
+    this.size = 220,
   });
 
   final String image;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: SizedBox(
-        width: 250,
-        height: 250,
+        width: size,
+        height: size,
         child: Image.asset(
           image,
           fit: BoxFit.cover,
