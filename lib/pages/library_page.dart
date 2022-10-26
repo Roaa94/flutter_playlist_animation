3d429644c56ae4212d63303b7ae245972b2bc3f1
roaa94 <roaa94khaddam@gmail.com>
2022-10-26 09:21:03 +0300

    refactor: Hero animation code clean up

--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -33,48 +33,14 @@ class LibraryPage extends StatelessWidget {
                   Animation<double> animation,
                   _,
                 ) {
-                  return PlaylistPage(
-                    routeAnimation: animation,
-                  );
+                  return PlaylistPage(routeAnimation: animation);
                 },
               ),
             );
           },
           child: Hero(
             tag: 'image-hero',
-            flightShuttleBuilder: (
-              BuildContext flightContext,
-              Animation<double> animation,
-              HeroFlightDirection flightDirection,
-              BuildContext fromHeroContext,
-              BuildContext toHeroContext,
-            ) {
-              final tween = Tween(
-                begin: HeroAnimationManager.startRotation,
-                end: HeroAnimationManager.endRotation,
-              );
-              final curvedAnimation = CurvedAnimation(
-                parent: animation,
-                curve: Curves.easeOut,
-              );
-              final rotationAnimation = tween.animate(curvedAnimation);
-
-              return AnimatedBuilder(
-                animation: rotationAnimation,
-                child: const ImageWrapper(
-                  image: 'assets/images/image-4.jpeg',
-                ),
-                builder: (context, child) {
-                  return Transform(
-                    transform: HeroAnimationManager.getTransformMatrix(
-                      rotationAnimation.value,
-                    ),
-                    alignment: Alignment.center,
-                    child: child,
-                  );
-                },
-              );
-            },
+            flightShuttleBuilder: HeroAnimationManager.flightShuttleBuilder,
             child: Transform(
               transform: HeroAnimationManager.startTransformMatrix,
               alignment: Alignment.center,
--- lib/utils/hero_animation_manager.dart
+++ lib/utils/hero_animation_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_playlist_animation/widgets/image_wrapper.dart';
 
 class HeroAnimationManager {
   static const double startRotation = 0.1;
@@ -16,4 +17,38 @@ class HeroAnimationManager {
   static Matrix4 startTransformMatrix = getTransformMatrix(startRotation);
 
   static Matrix4 endTransformMatrix = getTransformMatrix(-endRotation);
+
+  static Widget flightShuttleBuilder(
+    BuildContext flightContext,
+    Animation<double> animation,
+    HeroFlightDirection flightDirection,
+    BuildContext fromHeroContext,
+    BuildContext toHeroContext,
+  ) {
+    final tween = Tween(
+      begin: HeroAnimationManager.startRotation,
+      end: HeroAnimationManager.endRotation,
+    );
+    final curvedAnimation = CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeOut,
+    );
+    final rotationAnimation = tween.animate(curvedAnimation);
+
+    return AnimatedBuilder(
+      animation: rotationAnimation,
+      child: const ImageWrapper(
+        image: 'assets/images/image-4.jpeg',
+      ),
+      builder: (context, child) {
+        return Transform(
+          transform: getTransformMatrix(
+            rotationAnimation.value,
+          ),
+          alignment: Alignment.center,
+          child: child,
+        );
+      },
+    );
+  }
 }
